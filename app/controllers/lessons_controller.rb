44c1302819754b570f47ae5840db7197562b7fc7
Lizzie Koehler <lizziefkoehler@gmail.com>
2015-03-17 10:34:09 -0700

    add and pass capybara spec

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,29 @@
+class LessonsController < ApplicationController
+
+  def index
+    @lessons = Lesson.all
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      flash[:notice] = "Lesson Successfully Added!"
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def lesson_params
+      params.require(:lesson).permit(:name, :content)
+    end
+end
